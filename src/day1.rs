25afb18a2067de45f453fdb792cd35547b05f4f9
Mukul Agarwal <agarmukul23@gmail.com>
2024-12-04 22:16:51 -0500

    initial commit

---
+++ src/day1.rs
@@ -0,0 +1,7 @@
+#[aoc(day1, part1)]
+pub fn part1_bytes(input: &[u8]) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate aoc_runner_derive;
+extern crate aoc_runner;
+extern crate crypto;
+
+pub mod day1;
+
+aoc_lib! { year = 2024 }
