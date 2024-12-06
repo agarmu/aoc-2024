0a1bb75db5897b616a5f533cef6b522a78a9b66e
Mukul Agarwal <agarmukul23@gmail.com>
2024-12-05 22:08:09 -0500

    fix

--- everyone-codes/src/main.rs
+++ everyone-codes/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate aoc_runner;
+extern crate aoc_runner_derive;
+extern crate everyone_codes;
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = everyone_codes }
