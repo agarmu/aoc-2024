b8b720347fdce95bce9de6def30bbee06658b37d
Mukul Agarwal <agarmukul23@gmail.com>
2024-12-06 20:47:59 -0500

    remove everyone codes

--- everyone-codes/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#[macro_use]
-extern crate aoc_runner_derive;
-extern crate aoc_runner;
-extern crate pretty_assertions;
-
-pub mod task01;
-aoc_lib! { year = 0 }
--- everyone-codes/src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate aoc_runner;
-extern crate aoc_runner_derive;
-extern crate everyone_codes;
-
-use aoc_runner_derive::aoc_main;
-
-aoc_main! { lib = everyone_codes }
