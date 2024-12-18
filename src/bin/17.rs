c7eef92f11f2d9237094f386b0fc396c8167e170
Mukul Agarwal <agarmukul23@gmail.com>
2024-12-17 23:12:13 -0600

    day 17.2

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,53 +1,23 @@
-use itertools::Itertools;
-
 aoc_2024::solution!(17);
 
 type Reg = u64;
 #[derive(Debug, Clone)]
-struct Machine {
+struct Machine<'a> {
     a: Reg,
     b: Reg,
     c: Reg,
     ip: usize,
-    input: Vec<u8>,
-    output: Vec<Reg>,
+    rom: &'a [Reg],
 }
 
-impl Machine {
-    fn parse(input: &str) -> Option<Self> {
-        let (fst, snd) = input.split_once("\n\n")?;
-
-        let mut regvals = fst
-            .lines()
-            .map(|l| l.split_once(":").unwrap().1.trim())
-            .map(|x| x.parse().unwrap());
-
-        let a = regvals.next()?;
-        let b = regvals.next()?;
-        let c = regvals.next()?;
-
-        let input = snd
-            .trim()
-            .split_once(":")?
-            .1
-            .trim()
-            .split(",")
-            .map(|x| x.parse().unwrap())
-            .collect();
-
-        Some(Self {
-            a,
-            b,
-            c,
-            ip: 0,
-            input,
-            output: Vec::new(),
-        })
-    }
+impl<'a> Machine<'a> {
     #[inline]
     fn combo(&self, val: Reg) -> Reg {
         match val {
-            0..=3 => val as Reg,
+            0 => 0,
+            1 => 1,
+            2 => 2,
+            3 => 3,
             4 => self.a,
             5 => self.b,
             6 => self.c,
@@ -57,27 +27,29 @@ impl Machine {
     }
     #[inline]
     fn read(&mut self) -> Reg {
-        let v = self.input[self.ip] as Reg;
+        let v = self.rom[self.ip] as Reg;
         self.ip += 1;
         v
     }
 
     #[inline]
-    fn execute_instruction(&mut self) {
+    fn execute_instruction(&mut self, output: &mut Vec<Reg>) {
         let opcode = self.read();
         let operand = self.read();
         match opcode {
             0 => {
                 // adv
-                self.a >>= self.combo(operand);
+                let c = self.combo(operand);
+                self.a >>= c;
             }
             1 => {
                 // bxl
                 self.b ^= operand;
             }
             2 => {
+                let c = self.combo(operand);
                 // bst
-                self.b = self.combo(operand) % 8;
+                self.b = c % 8;
             }
             3 => {
                 // jnz
@@ -91,40 +63,84 @@ impl Machine {
             }
             5 => {
                 // output
-                let res = self.combo(operand) % 8;
-                self.output.push(res);
+                let c = self.combo(operand);
+                output.push(c % 8);
             }
             6 => {
                 // bdv
-                self.b = self.a >> self.combo(operand);
+                let c = self.combo(operand);
+                self.b = self.a >> c;
             }
             7 => {
-                // cdv
-                self.c = self.a >> self.combo(operand);
+                // adv
+                let c = self.combo(operand);
+                self.c = self.a >> c;
             }
             x => unreachable!("Invalid opcode {}", x),
         }
     }
 
-    fn simulate(&mut self) -> String {
-        while self.ip < self.input.len() {
-            self.execute_instruction();
+    fn simulate(&mut self) -> Vec<u64> {
+        let mut output = Vec::new();
+        while self.ip < self.rom.len() {
+            self.execute_instruction(&mut output);
         }
-        self.output.iter().map(Reg::to_string).join(",")
+        output
+    }
+
+    const fn new(a: Reg, b: Reg, c: Reg, rom: &'a [Reg]) -> Self {
+        let ip = 0;
+        Self { a, b, c, ip, rom }
     }
 }
 
-fn parse(input: &str) -> Option<Machine> {
-    Machine::parse(input)
+fn parse(input: &str) -> Option<(Reg, Reg, Reg, Vec<Reg>)> {
+    let (fst, snd) = input.split_once("\n\n")?;
+
+    let mut regvals = fst
+        .lines()
+        .map(|l| l.split_once(":").unwrap().1.trim())
+        .map(|x| x.parse().unwrap());
+
+    let a = regvals.next()?;
+    let b = regvals.next()?;
+    let c = regvals.next()?;
+
+    let input = snd
+        .trim()
+        .split_once(":")?
+        .1
+        .trim()
+        .split(",")
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    Some((a, b, c, input))
 }
 
 pub fn part_one(input: &str) -> Option<String> {
-    Some(parse(input)?.simulate())
+    let (a, b, c, rom) = parse(input)?;
+    let mut machine = Machine::new(a, b, c, &rom);
+    Some(format!("{:?}", machine.simulate()))
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let input = parse(input);
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, _, _, rom) = parse(input)?;
+    let v = backtrack_search(&rom, rom.len() - 1, 0);
+    v.into_iter().min()
+}
+
+fn backtrack_search(rom: &[u64], idx: usize, a: u64) -> Vec<u64> {
+    let k = (0..8).map(move |x| a * 8 + x).filter(move |a| {
+        let mut m = Machine::new(*a, 0, 0, rom);
+        m.simulate() == &rom[idx..]
+    });
+    if idx == 0 {
+        k.collect()
+    } else {
+        k.flat_map(move |a| backtrack_search(rom, idx - 1, a).into_iter())
+            .collect()
+    }
 }
 
 #[cfg(test)]
@@ -136,11 +152,5 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&aoc_2024::template::read_file("examples", DAY));
         assert_eq!(result, Some("4,6,3,5,6,3,5,2,1,0".to_owned()));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&aoc_2024::template::read_file("examples", DAY));
-        assert_eq!(result, None);
     }
 }
